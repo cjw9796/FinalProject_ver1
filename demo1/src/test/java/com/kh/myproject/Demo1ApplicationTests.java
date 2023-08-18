0850ee444c60271071cd93b1f3c6365a503f8c36
nagnebro <goldfish2679@naver.com>
2023-08-18 19:47:18 +0900

    first commit

---
+++ demo1/src/test/java/com/kh/myproject/Demo1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kh.myproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Demo1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
