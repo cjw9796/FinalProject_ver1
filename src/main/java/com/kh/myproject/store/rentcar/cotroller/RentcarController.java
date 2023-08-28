8ea5e89a377125314466455a4baec963c17286f4
nagnebro <goldfish2679@naver.com>
2023-08-28 15:41:41 +0900

    세션추가

--- src/main/java/com/kh/myproject/store/rentcar/cotroller/RentcarController.java
+++ src/main/java/com/kh/myproject/store/rentcar/cotroller/RentcarController.java
@@ -2,8 +2,10 @@ package com.kh.myproject.store.rentcar.cotroller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.SessionAttributes;
 
 @Controller
+@SessionAttributes("user")
 public class RentcarController {
 
 
--- src/main/java/com/kh/myproject/store/tour/contoller/TourController.java
+++ src/main/java/com/kh/myproject/store/tour/contoller/TourController.java
@@ -6,10 +6,12 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.servlet.ModelAndView;
 
 @RestController
 @Slf4j
+@SessionAttributes("user")
 public class TourController {
 
 
